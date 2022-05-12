972bba95452cc5c84a3943cc43b64aff82d78a7e
03Kristian <cristian.jacanamijoy@misena.edu.co>
2022-05-12 18:25:25 -0500

    first commit

---
+++ database/migrations/2022_05_12_201727_create_quotes_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->Time('hora_programada');
+            $table->time('hora_inicio');
+            $table->time('hora_fin');
+            $table->time('tiempo_total');
+            
+/*             $table->dateTime('tiempo_total')->nullable()->default(new DateTime());*/   
+            $table->float('pago',12,2);
+
+            $table->unsignedBigInteger('employee_id');
+            $table->foreign("employee_id")->references("id")->on("employee")
+            ->onUpdate("cascade")->onDelete("cascade");
+            
+            $table->unsignedBigInteger('waypay_id');
+            $table->foreign("waypay_id")->references("id")->on("way_pay")->onUpdate("cascade")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+};
